7ee18342ed6e329f84a7e808557956bf609487f3
Hariom333 <hariomrajpoot333@gmail.com>
2022-06-11 21:02:06 +0530

    project initalize

---
+++ google/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>lets go google clone</p>
+    </div>
+  );
+}
+
+export default App;
+
+/*
+context api, material ui, react router, goggle custom apu,
+make custom hook,
+use firebase 
+*/
